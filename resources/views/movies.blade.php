5b6cb08437d6b5a0c2030e2df47be837263d509a
Fabio Masciarelli <158151713+FabioMasciarelli@users.noreply.github.com>
2024-06-10 16:15:44 +0200

    add: custom style

--- resources/views/movies.blade.php
+++ resources/views/movies.blade.php
@@ -1,14 +1,16 @@
 @extends('layouts.app')
 
 @section('content')
+
+<div class="container-movies">
     <h2>Movies</h2>
 
     @foreach ($movies as $movie)
-        <div class="card">
-            <div>
+        <div class="ms_card">
+            <div class="box-img">
                 <img src="{{ $movie->image }}" alt="">
             </div>
-            <div>
+            <div class="info">
                 <h3>{{ $movie->title }}</h3>
                 <h6>{{$movie->original_title }}</h6>
                 <h6>{{ $movie->nationality }}</h6>
@@ -16,4 +18,6 @@
             </div>
         </div>
     @endforeach
+
+</div>
 @endsection
